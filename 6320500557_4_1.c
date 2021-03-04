d898f424251d87641f5c418fd049e95db09f9e5c
์Natnicha Phriksen <natnicha.phr@ku.th>
2021-03-04 17:15:20 +0700

    add3

--- 6320500557_4_1.c
+++ 6320500557_4_1.c
@@ -11,5 +11,13 @@ int main()
             scanf("%d",&coins[i][j]);
         }
     }
+     int total=0,max=0,min=0,maxx=0,minn=0;
+    for(i=0; i<n-2 ;i++)
+    {
+        for(j=i; j<i+3; j++)
+        {
+            total += (coins[j][0]*4)+(coins[j][1]*2)+(coins[j][2]*1);
+        }
+    }
     return 0;
 }
--- 6320500557_4_4.c
+++ 6320500557_4_4.c
@@ -2,7 +2,7 @@
 #include <string.h>
 int main()
 {
-    char word[1000],x[]={'i','of',"the","on","at","for","with",'a',"an","in","and"};
+    char word[1001],x[][]={'i','of',"the","on","at","for","with",'a',"an","in","and"};
     scanf("%s",word);
     char *word2;
     word2 = strstr(word,x);
