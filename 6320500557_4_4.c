e1d66d6af088ebc62945632d7084cfee73cf5e3d
์Natnicha Phriksen <natnicha.phr@ku.th>
2021-03-04 17:01:20 +0700

    edit

--- 6320500557_4_4.c
+++ 6320500557_4_4.c
@@ -2,7 +2,13 @@
 #include <string.h>
 int main()
 {
-    char word[100];
+    char word[1000],x[]={'i','of',"the","on","at","for","with",'a',"an","in","and"};
     scanf("%s",word);
+    char *word2;
+    word2 = strstr(word,x);
+    if(word2)
+    {
+        printf("%s",word2);
+    }
     return 0;
 }
